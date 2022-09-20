cbe2c7c3c6d99d5f0708a247fa49a2bd8dbc1bda
Victor Kornienko <raidhorr@gmail.com>
2022-09-20 08:18:52 +0300

    пагинация

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -15,6 +15,7 @@ class ProductsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+    paginate_by = 2
 
 
 class ProductDetail(DetailView):
