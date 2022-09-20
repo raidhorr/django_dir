b1bb0d98b56b184d1ef98d80d9f8bff100da9272
Victor Kornienko <raidhorr@gmail.com>
2022-09-20 19:45:56 +0300

    CRUD формы

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView, DetailView
-from .models import Product
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+
 from .filters import ProductFilter
+from .forms import ProductForm
+from .models import Product
 
 
 class ProductsList(ListView):
@@ -10,22 +15,13 @@ class ProductsList(ListView):
     context_object_name = 'products'
     paginate_by = 2
 
-    # Переопределяем функцию получения списка товаров
     def get_queryset(self):
-        # Получаем обычный запрос
         queryset = super().get_queryset()
-        # Используем наш класс фильтрации.
-        # self.request.GET содержит объект QueryDict, который мы рассматривали
-        # в этом юните ранее.
-        # Сохраняем нашу фильтрацию в объекте класса,
-        # чтобы потом добавить в контекст и использовать в шаблоне.
         self.filterset = ProductFilter(self.request.GET, queryset)
-        # Возвращаем из функции отфильтрованный список товаров
         return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Добавляем в контекст объект фильтрации.
         context['filterset'] = self.filterset
         return context
 
@@ -34,3 +30,21 @@ class ProductDetail(DetailView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'product'
+
+
+class ProductCreate(CreateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+# Представление удаляющее товар.
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
